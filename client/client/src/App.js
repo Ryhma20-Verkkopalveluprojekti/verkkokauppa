6165bb603b0a87952c87fddb1f931855e1660cd0
laurasavoila <n2sala00@students.oamk.fi>
2023-12-06 09:03:32 +0200

    korjaus

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -8,7 +8,7 @@ import Cart from './components/pages/Cart';
 import Shop from './components/pages/Shop';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Footer from './components/inc/Footer';
-/*import axios from "axios";
+/*import axios from "axios"; */
 
 function App() {
   return (
