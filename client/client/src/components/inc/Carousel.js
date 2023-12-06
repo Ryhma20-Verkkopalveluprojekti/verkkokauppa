a9d1bbfd01104c9f6c07d9d680ad179918d698ef
Lauraanni <n2ahla00@students.oamk.fi>
2023-12-06 14:49:37 +0200

    karusellikuvat

--- client/client/src/components/inc/Carousel.js
+++ client/client/src/components/inc/Carousel.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Image1 from '../images/kasviasetelma.jpg';
-import Image2 from '../images/kasviasetelma2.jpg';
-import Image3 from '../images/kultakoynnoskuva.jpg';
+import Image1 from '../images/kasviasetelmaUUSI.jpg';
+import Image2 from '../images/kasviasetelma2UUSI.jpg';
+import Image3 from '../images/kultakoynnoskuvaUUSI.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../pages/styles/Carousel.css';
