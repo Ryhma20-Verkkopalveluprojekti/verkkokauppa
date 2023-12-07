8d3506257a880c7461beeb25956404cc57e432bc
Lauraanni <n2ahla00@students.oamk.fi>
2023-12-07 10:24:13 +0200

    kuvakaruselli laitettu pyörimään automaattisesti

--- client/client/src/components/inc/Carousel.js
+++ client/client/src/components/inc/Carousel.js
@@ -6,31 +6,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../pages/styles/Carousel.css';
 
-function CarouselImages(){
-  return(
-    
-    <div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={Image1} class="d-block w-100" alt="..."/>
+function CarouselImages() {
+  return (
+    <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={Image1} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={Image2} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={Image3} className="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="carousel-item">
-      <img src={Image2} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={Image3} class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-   );
+  );
 }
 
 export default CarouselImages;
