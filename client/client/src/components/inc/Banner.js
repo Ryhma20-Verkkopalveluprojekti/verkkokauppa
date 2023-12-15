d847fc6a71f50794f26ea6dc3e31288641b9cefd
Lauraanni <n2ahla00@students.oamk.fi>
2023-12-15 22:49:10 +0200

    bannerin korjaus koko sivun leveydelle

--- client/client/src/components/inc/Banner.js
+++ client/client/src/components/inc/Banner.js
@@ -3,10 +3,15 @@
 import React from "react";
 import banner from '../images/banner.jpg';
 
+const bannerStyle = {
+    width: '100%',
+    height: 'auto',
+};
+
 function Banner() {
     return(
 <div>
-      <img src={banner} className="img-fluid" alt="..."/>
+         <img src={banner} style={bannerStyle} alt="Banner" />
 
 </div>
 
